b2188d4d49994716966571d7857a5baf966e35ef
grusha <124006415+grushello@users.noreply.github.com>
2025-09-29 07:59:47 +0000

    Completed lab 3

--- src/lab3_task1.c
+++ src/lab3_task1.c
@@ -51,20 +51,31 @@ int array_min(int arr[], int size) {
             my_min = arr[i];
         }
     }
-    return my_min; // placeholder
+    return my_min;
 }
 
 int array_max(int arr[], int size) {
-    // TODO: return largest element
-    return 0; // placeholder
+    int my_max = INT_MIN; //set to max infinity
+    for (int i=0;i<size;i++) {
+        if (my_max < arr[i]) {
+            my_max = arr[i];
+        }
+    }
+    return my_max;
 }
 
 int array_sum(int arr[], int size) {
-    // TODO: return sum of elements
-    return 0; // placeholder
+    int sum = 0;
+    for (int i = 0; i < size; ++i) {
+        sum += arr[i];
+    }
+    return sum;
 }
 
 float array_avg(int arr[], int size) {
-    // TODO: return average as float
-    return 0.0f; // placeholder
+    if(size == 0)
+    {
+        return 0;
+    }
+    return (float)(array_sum(arr, size)/size);
 }
